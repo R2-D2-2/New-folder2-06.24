1a48fd4ff900fbb45834aa08b612ab770892935a
R2-D2-2 <a1998vk@yandex.ru>
2024-06-29 12:40:48 +0300

    commit

--- 1/1.c
+++ 1/1.c
@@ -4,33 +4,34 @@ int main (void) // main function
 {   
     int gold = 15;
     int hp_goblin = 3;
-    char attack;
+    char action;
 
-    printf("You attacked by goblin. Goblin HP: %d\n", hp_goblin);
+    printf("You attacked by goblin. (A) to attack or (R) to run\n", hp_goblin);
 
-    attack = getchar();
+    action = getchar();
 
-    if (attack == 'a')
+    while (action != 'r')
+    {
+        if (action == 'a' || action == 'A')
     {
         hp_goblin = hp_goblin - 1;
         printf("You kick goblin. Goblin HP: %d\n", hp_goblin);
     }
 
-    printf("You pressed: %c\n", attack);
-
-    /*printf("You kick goblin. Goblin HP: %d\n", hp_goblin);
-
-    hp_goblin = hp_goblin - 1;*/
+    printf("You pressed: %c\n", action);
 
     if (hp_goblin < 1)
     {
         printf("You killed it.\n");
         printf("You get %d gold.\n", gold);
     }
-    else
-    {
-        printf("Goblin run away!\n");
+
+    printf("Next action?\n");
+    getchar(); // Clean buffer
+    action = getchar();
     }
+
+    printf("You've decided to run away! Good bye!\n");
 
     return 0;
 }
