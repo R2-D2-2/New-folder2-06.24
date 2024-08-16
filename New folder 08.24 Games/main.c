2a9f3873d8bb6864d8383e35419d0dbac2c6f76f
R2-D2-2 <a1998vk@yandex.ru>
2024-08-16 22:54:27 +0400

    1

--- New folder 08.24 Games/main.c
+++ New folder 08.24 Games/main.c
@@ -15,14 +15,60 @@ int main (void)
     printf ("Click 'A' left  or 'D' right.\n");
     choise = getchar();
 
-    if (choise == 'a')
+//-----------------------------
+
+    if (choise == 'a' || choise == 'A')
     {
-        printf ("You stand at the crossroads.\n");
-        printf ("Left or right?\n");
-    }
-    else
-    {
+        while (choise != 'd')
+        {
+            if (choise == 'a' || choise == 'A')
+            {
+                printf ("You stand at the crossroads.\n");
+                printf ("You turned right.\n");
+                printf ("Left or right?\n");
+            }
+            else if (choise == 'd' || choise == 'D')
+            {
+                choise = 'd';
+            }
+            else
+            {
+                printf ("You stand at the crossroads.\n");
+            }
+
+            getchar();
+            choise = getchar();
+        }
         printf ("Wasted\n");
+        return 0;
     }
-    return 0;
+
+//-----------------------------
+
+    if (choise == 'd' || choise == 'D')
+    {
+        while (choise != 'a')
+        {
+            if (choise == 'd' || choise == 'D')
+            {
+                printf ("You stand at the crossroads.\n");
+                printf ("You turned left.\n");
+                printf ("Left or right?\n");
+            }
+            else if (choise == 'a' || choise == 'A')
+            {
+                choise = 'a';
+            }
+            else
+            {
+                printf ("You stand at the crossroads.\n");
+            }
+
+            getchar();
+            choise = getchar();
+        }
+        printf ("Wasted\n");
+        return 0;
+    }
+
 }
